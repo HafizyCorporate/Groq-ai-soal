e03a4cbf258e09e94b2ac8c14d86f28ec9f50cd5
Teazha <hafizyazhar@gmail.com>
2026-02-05 16:02:39 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,20 @@
-const sqlite3 = require("sqlite3").verbose()
-const db = new sqlite3.Database("database.db")
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.db");
 
 db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT UNIQUE,
-      password TEXT
-    )
-  `)
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE,
+    password TEXT
+  )`);
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS history (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      user_id INTEGER,
-      soal TEXT,
-      jawaban TEXT,
-      foto TEXT,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `)
-})
+  db.run(`CREATE TABLE IF NOT EXISTS history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    soal TEXT,
+    jawaban TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )`);
+});
 
-module.exports = db
+module.exports = db;
