3e48ddd43e2e9e19c3337e3319a8b283eb617b48
Teazha <hafizyazhar@gmail.com>
2026-02-06 06:49:35 +0700

    Update db.js

--- db.js
+++ db.js
@@ -2,12 +2,14 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("database.db");
 
 db.serialize(() => {
+  // Tabel User
   db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT UNIQUE,
     password TEXT
   )`);
 
+  // Tabel History dengan kolom soal & jawaban terpisah
   db.run(`CREATE TABLE IF NOT EXISTS history (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
